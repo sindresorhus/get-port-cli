6ebf70780b9c1d50c615810722b2174930a0fa05
renjithspace <renjithrajeevvk@gmail.com>
2021-10-14 20:27:10 +0530

    Add cli func to avoid redundant execa cli call

--- test.js
+++ test.js
@@ -2,14 +2,16 @@ import test from 'ava';
 import execa from 'execa';
 import getPort from 'get-port';
 
+const cli = (args = []) => execa('./cli.js', args);
+
 test('cli', async t => {
-	const {stdout} = await execa('./cli.js');
+	const {stdout} = await cli();
 	t.true(Number(stdout) > 0);
 });
 
 test('cli with preferred port', async t => {
 	const port = await getPort();
-	const {stdout} = await execa('./cli.js', [port]);
+	const {stdout} = await cli([port]);
 	t.is(Number(stdout), port);
 });
 
@@ -17,6 +19,6 @@ test('cli with preferred ports and host', async t => {
 	const host = '127.0.0.1';
 	const port = await getPort({host});
 	const ports = [port, 80, 9090];
-	const {stdout} = await execa('./cli.js', [...ports, '-h', '127.0.0.1']);
+	const {stdout} = await cli([...ports, '-h', '127.0.0.1']);
 	t.true(ports.includes(Number(stdout)));
 });
